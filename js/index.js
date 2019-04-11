f57ed52e9dc450727992282041faa4c52429b5f0
Tgleason1980 <tglabon@gmail.com>
2019-04-10 21:33:02 -0500

    added content

--- js/index.js
+++ js/index.js
@@ -40,3 +40,54 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let headerimg = document.getElementById("cta-img");
+headerimg.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleimg = document.getElementById("middle-img");
+middleimg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+const navigation = document.querySelectorAll('a');
+navigation[0].textContent = siteContent['nav']['nav-item-1'] ;
+navigation[1].textContent = siteContent['nav']['nav-item-2'] ;
+navigation[2].textContent = siteContent['nav']['nav-item-3'] ;
+navigation[3].textContent = siteContent['nav']['nav-item-4'] ;
+navigation[4].textContent = siteContent['nav']['nav-item-5'] ;
+navigation[5].textContent = siteContent['nav']['nav-item-6'] ;
+
+
+navigation[0].style.color = "green";
+navigation[1].style.color = "green";
+navigation[2].style.color = "green";
+navigation[3].style.color = "green";
+navigation[4].style.color = "green";
+navigation[5].style.color = "green";
+
+const dom = document.querySelector('h1');
+dom.innerHTML = "DOM <br> Is <br> Awesome";
+
+const dombtn = document.querySelector('button');
+dombtn.textContent = "Get Started";
+
+const contentH4s = document.querySelectorAll('h4');
+contentH4s[0].textContent = siteContent['main-content']['features-h4'] ;
+contentH4s[1].textContent = siteContent['main-content']['about-h4'] ;
+contentH4s[2].textContent = siteContent['main-content']['services-h4'] ;
+contentH4s[3].textContent = siteContent['main-content']['product-h4'] ;
+contentH4s[4].textContent = siteContent['main-content']['vision-h4'] ;
+contentH4s[5].textContent = siteContent['contact']['contact-h4'] ;
+
+const content = document.querySelectorAll('p');
+content[0].textContent = siteContent['main-content']['features-content'] ;
+content[1].textContent = siteContent['main-content']['about-content'] ;
+content[2].textContent = siteContent['main-content']['services-content'] ;
+content[3].textContent = siteContent['main-content']['product-content'] ;
+content[4].textContent = siteContent['main-content']['vision-content'] ;
+content[4].textContent = siteContent['main-content']['vision-content'] ;
+const contactSheet = document.querySelectorAll('p');
+contactSheet[5].textContent = siteContent['contact']['address'] ;
+contactSheet[6].textContent = siteContent['contact']['phone'] ;
+contactSheet[7].textContent = siteContent['contact']['email'] ;
+
+const foot = document.querySelector('footer');
+foot.textContent = "Copyright Great Idea! 2018";
